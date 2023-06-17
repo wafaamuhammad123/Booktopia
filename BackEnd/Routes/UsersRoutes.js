16fe9363711fb12a6466583d1eb3adbb1ffd65ee
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-06-17 18:37:13 +0300

    edit error

--- BackEnd/Routes/UsersRoutes.js
+++ BackEnd/Routes/UsersRoutes.js
@@ -9,7 +9,7 @@ const admin = require("../permissions/userMWPermissions.js");
 const authUser= require("../permissions/auth.js");
 
 router.get("/users", admin, usersController.getAllUsers);
-router.post("/create",uploadProduct,usersController.addNewUser);
+router.post("/create",usersController.addNewUser);
 router.put("/user/:id", authUser, usersController.updateUser);
 router.post("/login", usersController.login);
 router.delete("/delete/:id", admin, usersController.DeleteUser);
