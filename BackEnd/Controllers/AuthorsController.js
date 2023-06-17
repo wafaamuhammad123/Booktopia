8d5079fd6d3038a0ade29b55a804a7ba38bb7e1c
Maysara Safwat <maisarasafwat2@gmail.com>
2023-06-17 11:57:31 +0300

    add create and update author

--- BackEnd/Controllers/AuthorsController.js
+++ BackEnd/Controllers/AuthorsController.js
@@ -13,10 +13,45 @@ let getAuthorById = async (req, res) => {
 };
 
 let createAuthor = async (req, res) => {
- 
+  try {
+    const { name, aboutHim} =  req.body;
+    const imageFile = req.files["image"][0].filename;
+    const author= new authorsModel({
+     name,
+     imageFile,
+     aboutHim
+    });
+    const savedAuthor= await author.save();
+
+    res.status(200).json({
+      message: "Author added Successfuly",
+      book: savedAuthor,
+    });
+  } catch (error) {
+    console.error("Error adding author:", error);
+    res.status(422).json({ error: "Failed to add author" });
+  }
+
 };
 
 let updateAuthor = async (req, res) => {
+
+
+try{
+  let authorID = req.params.id;
+   console.log(req.body);
+  let author= await authorsModel.findOneAndUpdate(
+      { _id: authorID},
+      req.body,
+      { new: true}
+    );
+
+  if (!author) {
+    throw new NotFoundError(`Author Not Found`);
+  }
+}catch (err) {
+    res.status(400).json({ msg: "Could not update Author" + err.message });
+  }
  
 };
 
