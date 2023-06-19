d1b35c99f790e382c1cee3556434141255477a4f
Samir <ahmedsamir25999@gmail.com>
2023-06-19 22:22:37 +0300

    sui

--- BackEnd/Routes/UsersRoutes.js
+++ BackEnd/Routes/UsersRoutes.js
@@ -1,27 +1,19 @@
-const express = require("express");
-const router = express.Router();
-const usersController = require("../Controllers/UsersController.js");
-const userValidation = require("../Utils/userValidation");
-const cors = require("cors");
-const multer = require("multer");
-//for Authorization
-const admin = require("../permissions/userMWPermissions.js");
-const authUser= require("../permissions/auth.js");
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
-    },
-  });
-  const upload = multer({ storage: storage });
+  const express = require("express");
+  const router = express.Router();
+  const usersController = require("../Controllers/UsersController.js");
+  const userValidation = require("../Utils/userValidation");
+  const cors = require("cors");
+  const multer = require("multer");
+  //for Authorization
+  const admin = require("../permissions/userMWPermissions.js");
+  const authUser= require("../permissions/auth.js");
 
-router.get("/users", admin, usersController.getAllUsers);
-router.post("/create",usersController.addNewUser);
-router.put("/user/:id", authUser, upload.single("image"), usersController.updateUser);
-router.post("/login", usersController.login);
-router.delete("/delete/:id", admin, usersController.DeleteUser);
-router.get("/:id", authUser, usersController.getUserById);
 
-module.exports = router;
+  router.get("/users", admin, usersController.getAllUsers);
+  router.post("/create",usersController.addNewUser);
+  router.put("/user/:id", authUser, upload.single("image"), usersController.updateUser);
+  router.post("/login", usersController.login);
+  router.delete("/delete/:id", admin, usersController.DeleteUser);
+  router.get("/:id", authUser, usersController.getUserById);
+
+  module.exports = router;
