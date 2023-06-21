a8397a9b140a97da70e16d983c104c9a48e9bb63
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-06-21 18:18:34 +0300

    nav & footer

--- FrontEnd/src/Components/header/header.js
+++ FrontEnd/src/Components/header/header.js
@@ -1,59 +1,166 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './header.css'
 import '../../style.css'
 import { NavLink , useNavigate} from "react-router-dom";
 import { logout} from '../../auth'
+import jwtDecode from 'jwt-decode';
+import {fetchuserDetails} from '../../api';
+
+
 const isAuthenticated = () => {
     const token = localStorage.getItem("token");
     if(token) return true;
     else return false;
   };
 
-const Header = () => {
+const Header = ({ userId }) => {
+    const [user, setUser] = useState(null);
+    const token=localStorage.getItem("token");
     const navigate = useNavigate();
     const handleLogout = () => {
       logout();
       navigate('/');
+      return(false);
     };
-    return (
-<header id="header">
-<div className="container">
-    <div className="row">
-        <div className="col-md-2">
-            <div className="main-logo">
-                <img src="images/main-logo.png" alt="logo_1" />
-            </div>
-        </div>
-        <div className="offset-2 col-md-8">
-            <nav id="navbar">
-                <div className="main-menu">
-                        <span className="menu-item"><NavLink className="link2" activeclassname="active2" to={`/home`}>Home</NavLink></span>
-                        <span className="menu-item"><NavLink className="link2" activeclassname="active2" to={`/allBooks`}>Books</NavLink></span>
-                        <span className="menu-item"><NavLink className="link2" activeclassname="active2" to={`/`}>Authors</NavLink></span>
-                        <span className="menu-item"><NavLink className="link2" activeclassname="active2" to={`/`}>Contact</NavLink></span>
-                        <span className="menu-item"><NavLink className="link2" activeclassname="active2" to={`/`}>About</NavLink></span>
-                        {isAuthenticated() ? (
-                        <span className="menu-item ">
-                            <div>
-                            <NavLink className="link2" activeclassname="active2" to={`/userprofile`}>
-                            Profile
-                            </NavLink>
-                            <div style={{display:"inline"}} onClick={handleLogout} className='ms-5 ' id="logout">Logout</div>
-                            </div>
-                        </span>
-                        ) : (
-                        <span className="menu-item ms-4">
-                            <NavLink id="loginBtn" activeclassname="active2" to={`/login`}>
-                            Login
-                            </NavLink>
-                        </span>
-                        )}
-                       
+
+    if(token){
+        const decodedToken = jwtDecode(token);
+        userId = decodedToken.userId;
+        console.log(userId);
+    }
+
+    useEffect(() => {
+        fetchuserDetails(userId)
+        .then((data) => {
+        setUser(data);
+        console.log(data)
+        })
+        .catch((err) => {
+        console.log(err);
+        });
+    }, [userId]);
+    if(!user){console.log(user, "login please")}
+
+        return (
+            <header id="header" className='navbar navbar-expand-lg' style={{textAlign:"center"}}>
+                <div className='nav-con'>
+                    <div className="container row  d-none d-lg-flex">
+                        <a className="navbar-brand col-lg-2">
+                            <img src="images/main-logo (1).png" alt="Logo"/>
+                        </a>
+                        <div className='col-lg-2'></div>
+                        <div className="collapse navbar-collapse col-md-auto row" id="collapsibleNavbar">
+                            <ul className="navbar-nav justify-content-end d-none d-lg-flex">
+                                <li className="nav-item col">
+                                    <a className="nav-link"><NavLink className="link2" activeclassname="active2" to={`/home`}>Home</NavLink></a>
+                                </li>
+                                <li className="nav-item col">
+                                    <a className="nav-link"><NavLink className="link2" activeclassname="active2" to={`/allBooks`}>Books</NavLink></a>
+                                </li>
+                                <li className="nav-item col">
+                                    <a className="nav-link"><NavLink className="link2" activeclassname="active2" to={`/`}>Authors</NavLink></a>
+                                </li>
+                                <li className="nav-item col">
+                                    <a className="nav-link"><NavLink className="link2" activeclassname="active2" to={`/`}>Contact</NavLink></a>
+                                </li>
+                                <li className="nav-item col">
+                                    <a className="nav-link"><NavLink className="link2" activeclassname="active2" to={`/`}>About</NavLink></a>
+                                </li>
+
+                                {user ? (
+
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <img
+                                            className="userImage"
+                                            src={user.image}
+                                            alt="User Image"
+                                            />
+                                        </a>
+                                        <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                            <li><a className="dropdown-item">
+                                                <NavLink className="link2" activeclassname="active2" to={`/userprofile`}>
+                                                Profile
+                                                </NavLink>
+                                            </a></li>
+                                            <li><a className="dropdown-item">
+                                                <div onClick={handleLogout} id="logout">Logout</div>
+                                            </a></li>
+                                        </ul>
+                                        </li>
+                                )
+                                : (
+                                    <li className="nav-item col">
+                                        <a className="nav-link">
+                                            <NavLink id="loginBtn" className="link2" activeclassname="active2" to={`/login`}>
+                                            Login
+                                            </NavLink>
+                                        </a>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className='d-flex d-lg-none container'>
+                        <div className='container'>
+                            <a className="navbar-brand col-lg-2">
+                                <img src="images/main-logo (1).png" alt="Logo"/>
+                            </a>
+                        </div>
+                        <ul className='row'>
+                            <button className="navbar-toggler" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            {user ? (
+                                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li>
+                                        <a className="dropdown-item">
+                                            <NavLink className="link2" activeclassname="active2" to={`/userprofile`}>
+                                            Profile
+                                            </NavLink>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item"><NavLink className="link2" activeclassname="active2" to={`/allBooks`}>Books</NavLink></a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item"><NavLink className="link2" activeclassname="active2" to={`/`}>Authors</NavLink></a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item"><NavLink className="link2" activeclassname="active2" to={`/`}>Contact</NavLink></a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item"><NavLink className="link2" activeclassname="active2" to={`/`}>About</NavLink></a>
+                                    </li>
+                                    <li><a className="dropdown-item">
+                                        <div onClick={handleLogout} id="logout">Logout</div>
+                                    </a></li>
+                                </ul>
+                            )
+                            : (
+                                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li>
+                                            <a className="dropdown-item">
+                                                <NavLink id="loginBtn" className="link2" activeclassname="active2" to={`/login`}>
+                                                Login
+                                                </NavLink>
+                                            </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item"><NavLink className="link2" activeclassname="active2" to={`/`}>About</NavLink></a>
+                                    </li>
+                                </ul>
+                            )}
+                        </ul>
+                    </div>
                 </div>
-            </nav>
-        </div>
-    </div>
-</div>
-</header>
-    )}
+            </header>
+        )
+    }
+
 export default Header;
+
